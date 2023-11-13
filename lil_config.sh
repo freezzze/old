28cdc332ab8299a74ae6d1096b8c72b8b9d20fa9
freezzze <rada611@gmail.com>
2023-11-13 16:21:05 +0300

    Update lil_config.sh

--- lil_config.sh
+++ lil_config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #скрипт настраивает работу от имени пользователя ubuntu без ввода пароля и правит ip-адрес и имя хоста (запрашивает)
+
 read -p "Введите IP-адрес: " ip_address
 read -p "Введите имя хоста: " host_name
+
 echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
 sudo sed -i "s/10.221.12.181\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
 sudo sed -i "s/master1/$host_name/" /etc/hosts
