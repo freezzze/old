48bde7f8d59ca114a2e775242a40a86dc991f185
freezzze <rada611@gmail.com>
2023-11-13 16:20:01 +0300

    Update lil_config.sh

--- lil_config.sh
+++ lil_config.sh
@@ -3,5 +3,5 @@
 read -p "Введите IP-адрес: " ip_address
 read -p "Введите имя хоста: " host_name
 echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
-sudo sed -i 's/10.221.12.181\/24/$ip_address\/24/' /etc/netplan/00-installer-config.yaml
-sudo sed -i 's/master1/$host_name/' /etc/hosts
+sudo sed -i "s/10.221.12.181\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
+sudo sed -i "s/master1/$host_name/" /etc/hosts
