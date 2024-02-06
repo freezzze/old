e586b9979a969c32a1c7b6df814ba7e880e0facb
freezzze <rada611@gmail.com>
2024-02-06 09:36:32 +0300

    Update lil_config.sh

--- lil_config.sh
+++ lil_config.sh
@@ -5,7 +5,7 @@
 ip_address=$1
 host_name=$2
 
-echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
+#echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
 sudo hostnamectl hostname $host_name
 sudo sed -i "s/10.221.12.181\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
 sudo netplan apply
