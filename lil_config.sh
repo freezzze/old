6cfa98d6ebc2b7a4aea912e0aa9215b74207c01c
freezzze <rada611@gmail.com>
2024-03-25 12:08:55 +0300

    Update lil_config.sh

--- lil_config.sh
+++ lil_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #скрипт настраивает работу от имени пользователя ubuntu без ввода пароля и правит ip-адрес и имя хоста (запрашивает)
-#запускать так (передаём значения напрямую) 'curl https://raw.githubusercontent.com/freezzze/old/main/lil_config.sh | sudo bash -s 10.221.12.182 master2'
+#запускать так (передаём значения напрямую) 'curl https://raw.githubusercontent.com/freezzze/old/main/lil_config.sh | sudo bash -s 10.221.12.182 node2'
 
 ip_address=$1
 host_name=$2
 
 #echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
 sudo hostnamectl hostname $host_name
-sudo sed -i "s/10.221.12.181\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
+sudo sed -i "s/10.221.12.189\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
 sudo netplan apply
