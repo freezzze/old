de5158ed4c9009f83bab24f1797ded5dbdbd91de
freezzze <rada611@gmail.com>
2024-10-15 17:13:13 +0300

    Update lil_config.sh

--- lil_config.sh
+++ lil_config.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 # Скрипт настраивает работу системы от имени пользователя 'ubuntu' без ввода пароля и правит ip-адрес и имя хоста (запрашивает)
 
-# Проверка наличия аргументов
-if [ $# -eq 2 ]; then
-    ip_address=$1
-    host_name=$2
-else
-    # Запрос ввода у пользователя
-    read -p "Введите IP-адрес: " ip_address
-    read -p "Введите имя хоста: " host_name
-fi
+echo "Введите ip адрес:"
+read ip_address
+#ip_address=$1
+echo "Введите hostname:"
+read host_name
+#host_name=$2
 
-# Убедитесь, что переменные не пустые
-if [ -z "$ip_address" ] || [ -z "$host_name" ]; then
-    echo "IP-адрес и имя хоста не могут быть пустыми."
-    exit 1
-fi
-
-# Настройка системы
-sudo hostnamectl set-hostname "$host_name"
+echo "ubuntu ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ubuntu
+sudo hostnamectl hostname $host_name
 sudo sed -i "s/10.221.12.181\/24/$ip_address\/24/" /etc/netplan/00-installer-config.yaml
 sudo netplan apply
